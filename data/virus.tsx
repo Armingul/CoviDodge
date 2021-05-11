0374432e5fe16d38cb57dd659c08bf2d3b8bdc4b
Carlos González Torres <45764371+Cargon16@users.noreply.github.com>
2021-05-11 12:21:00 +0200

    Fondo funcionando

---
+++ data/virus.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="covid" tilewidth="50" tileheight="50" tilecount="12" columns="6">
+ <image source="D:/Descargas/covidVerde.png" width="300" height="100"/>
+</tileset>
