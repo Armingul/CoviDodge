a70e79c83e4a25db1a3b64273fd3cb4459419f53
Carlos González Torres <45764371+Cargon16@users.noreply.github.com>
2021-05-19 13:51:59 +0200

    Modo dificil funcionando

--- javascript/practica3.js
+++ javascript/practica3.js
@@ -9,21 +9,21 @@ var game = function () {
         .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX,Audio")
         // Maximize this game to whatever the size of the browser is
         .setup({
-             maximize:true,
-             scaleToFit: true,
-             width: 1200,
-             height: 612
+            maximize: true,
+            scaleToFit: true,
+            width: 1200,
+            height: 612
 
         })
         // And turn on default input controls and touch input (for UI)
         .controls().touch().enableSound();
 
-//Q.debug = true;
+    //Q.debug = true;
 
-    Q.load("buttonHard.png, buttonEasy.png, buttonMedio.png, daddy.png, mario_small.json,covidAzul.png, covidVerde.png, virusA.json ,virusV.json, jerginga.png, hospital.png, button2.mp3, button2.ogg, main_music1.mp3, main_music1.ogg, hit3.mp3, hit3.ogg, music_level_complete.mp3, music_level_complete.ogg, ", function () {
-        
-    //Q.debug=true;
-     
+    Q.load("virusR.json, covidRojo.png, buttonHard.png, buttonEasy.png, buttonMedio.png, daddy.png, mario_small.json,covidAzul.png, covidVerde.png, virusA.json ,virusV.json, jerginga.png, hospital.png, button2.mp3, button2.ogg, main_music1.mp3, main_music1.ogg, hit3.mp3, hit3.ogg, music_level_complete.mp3, music_level_complete.ogg, ", function () {
+
+        //Q.debug=true;
+
         // Or from a .json asset that defines sprite locations
         Q.compileSheets("daddy.png", "mario_small.json");
         Q.animations('player_anim', {
@@ -88,10 +88,10 @@ var game = function () {
                 this.p.points[2] = [15, 25];
                 this.p.points[3] = [15, -25];
                 if (!this.p.dead) {
-        
-                    
-                    if(this.p.x != 50 && this.p.y != 380) this.p.gravity = 1;
-                    
+
+
+                    if (this.p.x != 50 && this.p.y != 380) this.p.gravity = 1;
+
                     if (this.p.y > 580 || this.p.y < 20) {
                         this.play("die");
                         this.p.dead = true;
@@ -139,28 +139,31 @@ var game = function () {
 
                     }
                 });
-     
+
             }
         });
 
-
-        Q.compileSheets("covidVerde.png", "virusV.json");
-        Q.animations('virus_animV', {
+        Q.compileSheets("covidRojo.png", "virusR.json");
+        Q.animations('virus_animR', {
             pulse: {
                 frames: [0, 1, 2],
                 rate: 1 / 2,
                 loop: true
             },
-            
+
         });
-        Q.Sprite.extend("covidVerde", {
+        Q.Sprite.extend("covidRojo", {
             init: function (p) {
                 this._super(p, {
-                    sprite: "virus_animV",
-                    sheet: "virus",
-                    vy: 0.1,
-                    range: 0,
-                    gravity:0,
+                    sprite: "virus_animR",
+                    sheet: "virusR",
+                     x: p.x,
+                    y: p.y,
+                    vy: -6,
+                    move: 'up',
+                    dead: false,
+                    range: p.range,
+                    gravity: 0,
                     dest: 0
                 });
                 this.add('2d,defaultEnemy,animation');
@@ -170,8 +173,53 @@ var game = function () {
                 this.p.points[1] = [-25, 25];
                 this.p.points[2] = [25, 25];
                 this.p.points[3] = [25, -25];
-            this.play("pulse")
-                
+                this.play("pulse");
+
+                if (this.p.move == 'up') {
+                    this.p.y += this.p.vy;
+                    this.p.x += this.p.vy;
+                }
+                if (this.p.y < 50) {
+                    this.p.move = 'down';
+                }
+                if (this.p.move == 'down') {
+                    this.p.y -= this.p.vy;
+                    this.p.x -= this.p.vy;
+                }
+                if (this.p.y > 530) {
+                    this.p.move = 'up';
+                }
+            }
+
+        });
+        Q.compileSheets("covidVerde.png", "virusV.json");
+        Q.animations('virus_animV', {
+            pulse: {
+                frames: [0, 1, 2],
+                rate: 1 / 2,
+                loop: true
+            },
+
+        });
+        Q.Sprite.extend("covidVerde", {
+            init: function (p) {
+                this._super(p, {
+                    sprite: "virus_animV",
+                    sheet: "virusV",
+                    vy: 0.1,
+                    range: 0,
+                    gravity: 0,
+                    dest: 0
+                });
+                this.add('2d,defaultEnemy,animation');
+            },
+            step: function (dt) {
+                this.p.points[0] = [-25, -25];
+                this.p.points[1] = [-25, 25];
+                this.p.points[2] = [25, 25];
+                this.p.points[3] = [25, -25];
+                this.play("pulse")
+
 
             }
         });
@@ -183,43 +231,43 @@ var game = function () {
                 rate: 1 / 2,
                 loop: true
             },
-        
-            
+
+
         });
 
         Q.Sprite.extend("covidAzul", {
             init: function (p) {
                 this._super(p, {
                     sprite: "virus_animA",
-                    sheet: "virus",
+                    sheet: "virusA",
                     x: p.x,
                     y: p.y,
                     vy: -6,
                     move: 'up',
                     dead: false,
                     range: p.range,
-                    gravity:0,
+                    gravity: 0,
                     dest: 0
                 });
                 this.add('2d,defaultEnemy,animation');
             },
             step: function (dt) {
                 this.p.points[0] = [-25, -25];
-                 this.p.points[1] = [-25, 25];
-                 this.p.points[2] = [25, 25]; 
+                this.p.points[1] = [-25, 25];
+                this.p.points[2] = [25, 25];
                 this.p.points[3] = [25, -25];
                 this.play("pulse");
 
-                if(this.p.move == 'up'){
-                this.p.y += this.p.vy;
+                if (this.p.move == 'up') {
+                    this.p.y += this.p.vy;
                 }
-                if(this.p.y < 50){
+                if (this.p.y < 50) {
                     this.p.move = 'down';
                 }
-                if(this.p.move == 'down'){
+                if (this.p.move == 'down') {
                     this.p.y -= this.p.vy;
                 }
-                if(this.p.y > 530){
+                if (this.p.y > 530) {
                     this.p.move = 'up';
                 }
             }
@@ -234,7 +282,7 @@ var game = function () {
                     asset: "jerginga.png",
                     x: 3500,
                     y: 350,
-                    gravity:0,
+                    gravity: 0,
                     win: false
                 });
                 this.add('2d');
@@ -280,7 +328,7 @@ var game = function () {
                 Q.stageScene('title-screen', 1);
                 Q.state.p.score = 0;
                 Q.audio.play('button2.mp3');
-              
+
             });
             container.fit(20);
         });
@@ -314,7 +362,7 @@ var game = function () {
                 Q.stageScene('title-screen', 1);
                 Q.state.p.score = 0;
                 Q.audio.play('button2.mp3');
-              
+
 
             });
             container.fit(20);
@@ -355,7 +403,7 @@ var game = function () {
                 y: 0,
                 x: 0,
                 fill: "#1C00ff00"
-                
+
             }));
             buttonMedio.on("click", function () {
                 Q.clearStages();
@@ -374,7 +422,7 @@ var game = function () {
                 y: 100,
                 x: 0,
                 fill: "#1C00ff00"
-                
+
             }));
             buttonDificil.on("click", function () {
                 Q.clearStages();
@@ -409,7 +457,7 @@ var game = function () {
 
 
         Q.scene("levelEasy", function (stage) {
-            
+
             Q.stageTMX("levelFacil.tmx", stage);
             // Create the player and add them to the stage
 
@@ -429,7 +477,7 @@ var game = function () {
 
 
         Q.scene("levelNormal", function (stage) {
-            
+
             Q.stageTMX("levelNormal.tmx", stage);
             // Create the player and add them to the stage
 
@@ -441,6 +489,25 @@ var game = function () {
             stage.insert(new Q.Princess());
         });
         Q.loadTMX("levelNormal.tmx", function () {
+            Q.state.reset({
+                score: 0
+            });
+            Q.stageScene("title-screen");
+        });
+
+        Q.scene("levelHard", function (stage) {
+
+            Q.stageTMX("levelDificil.tmx", stage);
+            // Create the player and add them to the stage
+
+            var player = stage.insert(new Q.Player());
+            stage.add("viewport").follow(player, {
+                x: true,
+                y: false
+            });
+            //stage.insert(new Q.Princess());
+        });
+        Q.loadTMX("levelDificil.tmx", function () {
             Q.state.reset({
                 score: 0
             });
